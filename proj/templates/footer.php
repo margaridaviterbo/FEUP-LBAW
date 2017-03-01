f7245605de552793e525014dba86f1c528074843
Catarina24 <aniratac.correia@gmail.com>
2017-03-01 23:46:20 +0000

    Changed stylesheet

--- proj/templates/footer.php
+++ proj/templates/footer.php
@@ -1,7 +1,6 @@
 
-        <footer class="container-fluid text-center">
-            <br> <br>
-            <em>LBAW 2016/17</em>
+        <footer class="container-fluid text-center navbar-fixed-bottom" id="footer">
+            <h5>LBAW 2016/17</h5>
         </footer>
 
     </body>
--- website/templates/header.php
+++ website/templates/header.php
@@ -7,11 +7,8 @@
         <title>Eventify</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Baloo" rel="stylesheet">
-        <link rel="stylesheet" href="../resources/css/reset.css">
         <link rel="stylesheet" href="../lib/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../resources/css/reset.css">
         <link rel="stylesheet" href="../resources/css/style.css">
     </head>
 
@@ -19,10 +16,20 @@
     <script src="../lib/bootstrap/js/bootstrap.min.js"></script>
 
     <header>
-        <div class="container-fluid text-center navbar-fixed-top" id="header">
+        <br>
+        <div class="container-fluid text-center">
             <div class="row">
                 <div class="col-sm-3">
-                    <h2>Eventify</h2>
+                    <table class="table">
+                        <tbody>
+                        <tr>
+                            <td><h2>Eventify</h2></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <div class="col-sm-6">
                     <form class="navbar-form navbar-center" role="search">
@@ -37,31 +44,10 @@
                     </form>
                 </div>
                 <div class="col-sm-3">
-                    <ul class="nav navbar-nav navbar-right" id="login">
+                    <ul class="nav navbar-nav navbar-right">
                         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                         <li><a href="#"><span class="glyphicon glyphicon-pencil"></span> Register</a></li>
                     </ul>
                 </div>
             </div>
         </div>
-    </header>
-
-    <div class="container-fluid text-left">
-        <div class="row">
-            <content class="col-sm-9">
-
-            </content>
-
-            <content class="col-sm-3">
-                <div class="aside-user-buttons">
-                    <div class="list-group">
-                        <a href="#" class="btn list-group-item">My Events</a>
-                        <a href="#" class="btn list-group-item">Events Atended</a>
-                        <a href="#" class="btn list-group-item">Invitations</a>
-                        <a href="#" class="btn list-group-item">Notifications</a>
-                    </div>
-                    <a href="#" class="btn btn-primary btn-lg">Create Event</a>
-                </div>
-            </content>
-        </div>
-    </div>
