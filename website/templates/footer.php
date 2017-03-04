68573cb82dee90ca63eb5b8e056c4c759dab14d9
LP <mieic1204994@fe.up.pt>
2017-03-04 17:43:20 +0000

    Added modal window.

--- website/templates/footer.php
+++ website/templates/footer.php
@@ -24,6 +24,22 @@
         <p>
           
           <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              </div>
+
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox"> Remember me
+                </label>
+              </div>
+
+            </form>
           
           </p>
       </div>
