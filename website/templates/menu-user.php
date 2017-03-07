07920472b71c5426aea5e2af467fb47d156af2da
Catarina24 <aniratac.correia@gmail.com>
2017-03-07 13:36:48 +0000

    header fixed

--- website/templates/menu-user.php
+++ website/templates/menu-user.php
@@ -8,22 +8,22 @@
 
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
 
-                    <div class="col-xs-offset-8 col-xm-1">
                         <span class="sr-only"></span>
 
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
-
-                    </div>
-
                 </button>
+
+                 <div class="hidden-sm hidden-md hidden-lg site-title">
+                     <a href="../index.php"><h2>Eventify</h2></a>
+                 </div>
 
             </div>
 
             <div class="row text-center">
 
-                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 site-title">
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-0 hidden-xs site-title">
                     <a href="../index.php"><h2>Eventify</h2></a>
                 </div>
 
