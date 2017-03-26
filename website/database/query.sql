8094008ee0b8611fc1884154137d2c5d5943b277
Catarina24 <aniratac.correia@gmail.com>
2017-03-26 18:07:33 +0100

    Added one more select...

--- website/database/query.sql
+++ website/database/query.sql
@@ -46,3 +46,54 @@ $$ LANGUAGE SQL;
 
 --Usage
 SELECT get_events_by_city('Vila Real') AS EventCity;
+
+
+/* Get Tickets Bought By User */
+
+DROP FUNCTION IF EXISTS get_user_tickets(userId integer);
+
+CREATE OR REPLACE FUNCTION get_user_tickets(userId integer) RETURNS SETOF integer AS $$
+SELECT public.ticket.ticket_id
+FROM public.ticket
+INNER JOIN public.users
+ON public.users.user_id = public.ticket.user_id
+WHERE public.users.user_id = userId;
+$$ LANGUAGE SQL;
+
+--Usage
+SELECT get_user_tickets(1) AS UserTickets;
+
+
+/* Get Events Attended By User */
+
+DROP FUNCTION IF EXISTS get_events_attended_by_user(userId integer);
+
+CREATE OR REPLACE FUNCTION get_events_attended_by_user(userId integer) RETURNS SETOF integer AS $$
+SELECT public.event.event_id
+FROM public.event
+INNER JOIN public.guest
+ON public.guest.event_id = public.event.event_id
+INNER JOIN public.authenticated_user
+ON public.authenticated_user.user_id = public.guest.user_id
+WHERE public.authenticated_user.user_id = userId AND public.guest.is_going = TRUE AND public.event.beginning_date < current_date;
+$$ LANGUAGE SQL;
+
+--Usage
+SELECT get_events_attended_by_user(1) AS attendedEvents;
+
+
+
+/* Get Notifications of Admin */
+
+DROP FUNCTION IF EXISTS get_admin_notifications(adminId integer);
+
+CREATE OR REPLACE FUNCTION get_admin_notifications(adminId integer) RETURNS SETOF integer AS $$
+SELECT public.notification.notification_id
+FROM public.notification
+INNER JOIN public.administrator
+ON public.administrator.administrator_id = public.notification.administrator_id
+WHERE public.administrator.administrator_id = adminId;
+$$ LANGUAGE SQL;
+
+--Usage
+SELECT get_admin_notifications(1) AS adminNotifications;
