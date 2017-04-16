151c507ec23916c44b2d5602f00ea20cc2a2a5af
LP <mieic1204994@fe.up.pt>
2017-04-16 05:14:18 +0100

    project "framewoked"

---
+++ website_gnomo/config/init.php
@@ -0,0 +1,30 @@
+<?php
+  session_set_cookie_params(3600, '...');
+  session_start();
+
+  $BASE_DIR = '...';
+  $BASE_URL = '...';
+
+  $conn = new PDO('pgsql:host=HOSTNAME;dbname=DATABASE', 'USER', 'PASSWORD');
+  $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  include_once($BASE_DIR . 'lib/smarty/libs/Smarty.class.php');
+  
+  $smarty = new Smarty;
+  $smarty->template_dir = $BASE_DIR . 'templates/';
+  $smarty->compile_dir = $BASE_DIR . 'templates_c/';
+  $smarty->assign('BASE_URL', $BASE_URL);
+  
+  $smarty->assign('ERROR_MESSAGES', $_SESSION['error_messages']);  
+  $smarty->assign('FIELD_ERRORS', $_SESSION['field_errors']);
+  $smarty->assign('SUCCESS_MESSAGES', $_SESSION['success_messages']);
+  $smarty->assign('FORM_VALUES', $_SESSION['form_values']);
+  $smarty->assign('USERNAME', $_SESSION['username']);
+  
+  unset($_SESSION['success_messages']);
+  unset($_SESSION['error_messages']);  
+  unset($_SESSION['field_errors']);
+  unset($_SESSION['form_values']);
+?>
+
