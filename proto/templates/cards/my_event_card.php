f3f86da2c11548aeed05cc843c7b40fe508bd994
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-16 17:02:02 +0100

    test

---
+++ proto/templates/cards/my_event_card.php
@@ -0,0 +1,29 @@
+<div class="container-fluid event-card-medium">
+    <p class="titulo-card">Sessão de Demonstração para o Desenvolvimento em Comunicação</p>
+    <div class="row">
+        <div class="col-sm-3">
+            <img src="../resources/images/2.jpg" />
+        </div>
+        <div class="col-sm-9">
+            <p class="text-card"> Quinta, 9 de Março às 19:45</p>
+            <p class="text-card">ISG</p>
+            <p class="text-card">Gratuito</p>
+            <div class="container-fluid">
+                <div class="row">
+
+                    <button onclick="window.location.href='../pages/show-event-page.php'" type="button" class="btn btn-default col-sm-5">See Event</button>
+                    <button onclick="window.location.href='../pages/my-page-edit-event.php'" type="button" class="btn btn-default col-sm-5">Edit Event</button>
+
+                    <div class="classifica-card col-sm-7">
+                        <i class="fa fa-star fa-2x" aria-hidden="true"></i>
+                        <i class="fa fa-star fa-2x" aria-hidden="true"></i>
+                        <i class="fa fa-star fa-2x" aria-hidden="true"></i>
+                        <i class="fa fa-star fa-2x" aria-hidden="true"></i>
+                        <i class="fa fa-star-o fa-2x" aria-hidden="true"></i>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
