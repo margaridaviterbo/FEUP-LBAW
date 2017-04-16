73237ea7a8a41b2c3363ba9d80494541806f8ede
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-16 12:15:16 +0100

    just an error

--- proto/pages/user/homepage.php
+++ proto/pages/user/homepage.php
@@ -13,32 +13,6 @@
         <?php include('../../templates/cards/homepage_event_card.php'); ?>
 		<?php include('../../templates/cards/homepage_event_card.php'); ?>
 		<?php include('../../templates/cards/homepage_event_card.php'); ?>
-          <div class="container-fluid event-card-medium">
-            <p class="titulo-card">Sessão de Demonstração para o Desenvolvimento em Comunicação</p>
-            <div class="row">
-              <div class="col-sm-3">
-                <img src="../resources/images/1.jpg" />
-              </div>
-              <div class="col-sm-9">
-                <p class="text-card"> Quinta, 9 de Março às 19:45</p>
-                <p class="text-card">ISG</p>
-                <p class="text-card">Gratuito</p>
-                <div class="container-fluid">
-                  <div class="row">
-                     <button onclick="window.location.href='show-event-page.php'" type="button" class="btn btn-default col-sm-5">See More...</button>
-                    <div class="classifica-card col-sm-7">
-                      <i class="fa fa-star fa-2x" aria-hidden="true"></i>
-                      <i class="fa fa-star fa-2x" aria-hidden="true"></i>
-                      <i class="fa fa-star fa-2x" aria-hidden="true"></i>
-                      <i class="fa fa-star-o fa-2x" aria-hidden="true"></i>
-                      <i class="fa fa-star-o fa-2x" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
         <div class="clearfix visible-md-block"></div>
 		<?php include('../../templates/cards/homepage_event_card.php'); ?>
 		<?php include('../../templates/cards/homepage_event_card.php'); ?>
@@ -47,32 +21,6 @@
 		<?php include('../../templates/cards/homepage_event_card.php'); ?>
 		<?php include('../../templates/cards/homepage_event_card.php'); ?>
 		<?php include('../../templates/cards/homepage_event_card.php'); ?>
-          <div class="container-fluid event-card-medium">
-            <p class="titulo-card">Sessão de Demonstração para o Desenvolvimento em Comunicação</p>
-            <div class="row">
-              <div class="col-sm-3">
-                <img src="../resources/images/1.jpg" />
-              </div>
-              <div class="col-sm-9">
-                <p class="text-card"> Quinta, 9 de Março às 19:45</p>
-                <p class="text-card">ISG</p>
-                <p class="text-card">Gratuito</p>
-                <div class="container-fluid">
-                  <div class="row">
-                     <button onclick="window.location.href='show-event-page.php'" type="button" class="btn btn-default col-sm-5">See More...</button>
-                    <div class="classifica-card col-sm-7">
-                      <i class="fa fa-star fa-2x" aria-hidden="true"></i>
-                      <i class="fa fa-star fa-2x" aria-hidden="true"></i>
-                      <i class="fa fa-star fa-2x" aria-hidden="true"></i>
-                      <i class="fa fa-star-o fa-2x" aria-hidden="true"></i>
-                      <i class="fa fa-star-o fa-2x" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
         <div class="clearfix visible-md-block"></div>
       </content>
     </div>
