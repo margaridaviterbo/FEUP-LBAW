48a3371549795981fc3f0cf35122aa3e7e9473cd
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-16 11:46:23 +0100

    added sql changes

--- Base_Dados/creates.sql
+++ Base_Dados/creates.sql
@@ -1,5 +1,6 @@
-DROP SCHEMA public CASCADE;
+DROP SCHEMA IF EXISTS public CASCADE;
 CREATE SCHEMA public;
+SET SCHEMA 'public';
 
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO public;
