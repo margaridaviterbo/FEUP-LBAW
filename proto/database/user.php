72db4eb7976f63df06b6f4baf24da9f553091ca1
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 02:57:17 +0100

    test

--- proto/database/user.php
+++ proto/database/user.php
@@ -8,9 +8,9 @@
   
    function getAllUsers($page, $name) {
     global $conn;
-    $stmt = $conn->prepare('SELECT  public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url
+    $stmt = $conn->prepare('SELECT  *
 							FROM public.Authenticated_User, public.Users
-							WHERE public.Authenticated_User.user_id = public.Users.user_id AND public.Users.first_name LIKE '%?%'
+							WHERE public.Users.first_name LIKE '%?%'
 							LIMIT 10 OFFSET ? * 10;');
     $stmt->execute(array($page, $name));
     return $stmt->fetchAll();
