b304a2994dbe38f9f16b0567b7fd1e7c8535eb66
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 19:07:35 +0100

    sdf

--- proto/resources/js/serch.js
+++ proto/resources/js/serch.js
@@ -9,12 +9,11 @@ function initserchname() {
 }
 
 function addeventChange() {
-	$('#serch-input').focus(changedContent());
-}
-
-function changedContent() {
+	$('#serch-input').focus(function() {
 	var name = $('#serch-input').val();
 	alert(name);
 	$('#searched-words').text(name);
 	$('#page-header').text("Search Results for \"" + name + "\"");
+});
 }
+
