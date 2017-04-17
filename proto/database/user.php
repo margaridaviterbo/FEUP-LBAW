0c8a300cf344ccf65286a88a0cebdda6eb321dc1
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 03:00:21 +0100

    test

--- proto/database/user.php
+++ proto/database/user.php
@@ -8,11 +8,14 @@
   
    function getAllUsers($page, $name) {
     global $conn;
+	echo 'vai';
     $stmt = $conn->prepare('SELECT  *
 							FROM public.Authenticated_User, public.Users
 							WHERE public.Users.first_name LIKE "%?%"
 							LIMIT 10 OFFSET ? * 10;');
+	echo 'asd';
     $stmt->execute(array($page, $name));
+	echo 'lol';
     return $stmt->fetchAll();
   }
 ?>
