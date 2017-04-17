340a5fb1b235b1c548b404448bbf4ce8965d281d
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 03:26:33 +0100

    test

--- proto/database/user.php
+++ proto/database/user.php
@@ -9,11 +9,11 @@
    function getSearchUsers($page, $name) {
     global $conn;
 
-    $stmt = $conn->prepare('SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url
+    $stmt = $conn->prepare('SELECT *
 							FROM public.Authenticated_User, public.Users
 							WHERE public.Users.first_name LIKE \'%?%\'
 							LIMIT 10 OFFSET ? * 10;');
-    $stmt->execute(array('a', 1));
+    $stmt->execute(array($name, $page));
     return $stmt->fetchAll();
   }
 ?>
