e44c127d543ef19b4f9507cd6ec3145bef4e4afa
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 03:32:06 +0100

    test

--- proto/database/user.php
+++ proto/database/user.php
@@ -11,9 +11,9 @@
 
     $stmt = $conn->prepare('SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url
 							FROM public.Authenticated_User, public.Users
-							WHERE public.Users.first_name LIKE \'%a%\'
+							WHERE public.Users.first_name LIKE \'%?%\'
 							LIMIT 10 OFFSET ? * 10;');
-    $stmt->execute(array($page));
+    $stmt->execute(array($name, $page));
     return $stmt->fetchAll();
   }
 ?>
