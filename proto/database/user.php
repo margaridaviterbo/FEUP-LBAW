3c63e106ec84f99afada01cf89c65613db066997
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 04:39:43 +0100

    test

--- proto/database/user.php
+++ proto/database/user.php
@@ -9,11 +9,11 @@
    function getSearchUsers($page, $name) {
     global $conn;
 	$param = "%$name%";
-    $stmt = $conn->prepare('SELECT first_name, last_name, email, photo_url
+    $stmt = $conn->prepare('SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url
 							FROM public.Authenticated_User INNER JOIN public.Users ON (public.Authenticated_User.user_id = public.Users.user_id)
-							WHERE first_name = \'Rui\'
-							LIMIT 10 OFFSET ( ? * 10);');
-    $stmt->execute(array($page));
+							WHERE (last_name LIKE ? OR first_name LIKE ?) 
+							LIMIT 10 OFFSET ? * 10;');
+    $stmt->execute(array($param, $param, $page));
     return $stmt->fetchAll();
   }
 ?>
--- proto/pages/user/search.php
+++ proto/pages/user/search.php
@@ -208,7 +208,7 @@
             </div>
 			
 			<?php 
-			print_r(getSearchUsers(1, 'u'));
+			print_r(getSearchUsers(0, 'u'));
 			?>
             <div class="container-fluid user-card-medium">
               <p class="titulo-card">festa123</p>
