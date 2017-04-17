66ad4a620c27412cb4c3f1360ce7184105dea1d3
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 03:59:51 +0100

    test

--- proto/database/user.php
+++ proto/database/user.php
@@ -11,7 +11,7 @@
 	$param = "%$name%";
     $stmt = $conn->prepare('SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url
 							FROM public.Authenticated_User INNER JOIN public.Users ON (public.Authenticated_User.user_id = public.Users.user_id)
-							WHERE public.Users.first_name LIKE ?
+							WHERE public.Users.last_name LIKE ?
 							LIMIT 10 OFFSET ? * 10;');
     $stmt->execute(array($param, $page));
     return $stmt->fetchAll();
