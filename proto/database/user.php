0d0f5cab6fa2fa4c2ebeaa9bc5453db8ac6c50b8
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 04:36:48 +0100

    test

--- proto/database/user.php
+++ proto/database/user.php
@@ -9,7 +9,7 @@
    function getSearchUsers($page, $name) {
     global $conn;
 	$param = "%$name%";
-    $stmt = $conn->prepare('SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url
+    $stmt = $conn->prepare('SELECT first_name, last_name, email, photo_url
 							FROM public.Authenticated_User INNER JOIN public.Users ON (public.Authenticated_User.user_id = public.Users.user_id)
 							WHERE first_name = \'Rui\'
 							LIMIT 10 OFFSET ? * 10;');
