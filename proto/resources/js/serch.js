070f48d5ef7f041a0a3b96c8867cf2cfffc27794
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 19:12:57 +0100

    wer

--- proto/resources/js/serch.js
+++ proto/resources/js/serch.js
@@ -10,10 +10,10 @@ function initserchname() {
 
 function addeventChange() {
 	$('#serch-input').focus(function() {
-		var name = $('#serch-input').val();
-		alert(name);
-		$('#searched-words').text(name);
-		$('#page-header').text("Search Results for \"" + name + "\"");
-	});
+	var name = $('#serch-input').val();
+	alert(name);
+	$('#searched-words').text(name);
+	$('#page-header').text("Search Results for \"" + name + "\"");
+});
 }
 
