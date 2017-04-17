3cc855446b144bd2b02b27d2b4febe2a4ee7bb18
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 02:55:25 +0100

    teest

--- proto/pages/user/search.php
+++ proto/pages/user/search.php
@@ -1,5 +1,4 @@
 <?php include_once('../../config/init.php'); ?>
-<?php include_once($BASE_DIR . 'database/user.php'); ?>
 <?php include($BASE_DIR . 'templates/header.php'); ?>
 <?php include($BASE_DIR . 'templates/menu-user.php'); ?>
 <?php include($BASE_DIR . 'templates/aside-menu.php'); ?>
