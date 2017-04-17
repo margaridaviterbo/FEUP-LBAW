f453d09fd9dca11288cadafdd09762a45e5a2ed1
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 19:31:57 +0100

    sef

--- proto/resources/js/serch.js
+++ proto/resources/js/serch.js
@@ -9,7 +9,7 @@ function initserchname() {
 }
 
 function addeventChange() {
-	$('#serch-input').change(function() {
+	$('#serch-input').on('input', function() {
 		var name = $('#serch-input').val();
 		alert(name);
 		$('#searched-words').text(name);
