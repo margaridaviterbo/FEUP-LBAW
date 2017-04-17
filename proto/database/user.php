a36680b3a34cf08d264426f8ee6ce9822b25f098
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 04:27:11 +0100

    test

--- proto/database/user.php
+++ proto/database/user.php
@@ -11,7 +11,7 @@
 	$param = "%$name%";
     $stmt = $conn->prepare('SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url
 							FROM public.Authenticated_User INNER JOIN public.Users ON (public.Authenticated_User.user_id = public.Users.user_id)
-							WHERE public.Users.first_name LIKE \'_u%\'
+							WHERE public.Users.first_name = \'Rui\'
 							LIMIT 10 OFFSET ? * 10;');
     $stmt->execute(array($page));
     return $stmt->fetchAll();
