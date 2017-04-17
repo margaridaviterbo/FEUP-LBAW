90380c00010ec1a2870395aa85f671cd9e799cd6
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 03:21:46 +0100

    teset

--- proto/database/user.php
+++ proto/database/user.php
@@ -16,7 +16,7 @@
 							FROM public.Authenticated_User, public.Users
 							WHERE public.Authenticated_User.user_id = public.Users.user_id AND public.Users.first_name LIKE \'%?%\'
 							LIMIT 10 OFFSET ? * 10;');*/
-	$stmt = $conn->prepare("SELECT * FROM public.Authenticated_User, public.Users WHERE public.Users.first_name LIKE '%c%' LIMIT 10 OFFSET 1 * 10;");
+	$stmt = $conn->prepare("SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url FROM public.Authenticated_User, public.Users WHERE public.Users.first_name LIKE '%c%' LIMIT 10 OFFSET 1 * 10;");
     $stmt->execute();
     return $stmt->fetchAll();
   }
