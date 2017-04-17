5f3e611cf06633bf36aa262cf9b60ddbbd04374a
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 04:30:16 +0100

    test

--- proto/database/user.php
+++ proto/database/user.php
@@ -10,7 +10,8 @@
     global $conn;
 	$param = "%$name%";
     $stmt = $conn->prepare('SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url
-							FROM public.Authenticated_User INNER JOIN public.Users ON (public.Authenticated_User.user_id = public.Users.user_id);');
+							FROM public.Authenticated_User INNER JOIN public.Users ON (public.Authenticated_User.user_id = public.Users.user_id)
+							WHERE first_name = \'Rui\';');
     $stmt->execute();
     return $stmt->fetchAll();
   }
