894f56c24b0a8121bb457068e08c63ff198e130f
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 02:26:10 +0100

    test

--- proto/pages/user/main.php
+++ proto/pages/user/main.php
@@ -1,5 +1,5 @@
 <?php
-	include_once($BASE_DIR . 'config/init.php');
+	include_once('../../config/init.php');
     include($BASE_DIR . 'templates/header.php');
     include($BASE_DIR . 'templates/menu-visitor.php');
     include($BASE_DIR . 'pages/user/homepage.php');
