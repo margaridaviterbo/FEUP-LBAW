2d72499b0b6efe0c2257e14c9adfd0b6060b80c7
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 23:06:23 +0100

    asd

--- proto/resources/js/serch.js
+++ proto/resources/js/serch.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   initserchname();
   addeventChange();
 });
+BASE_URL = 'http://gnomo.fe.up.pt/~lbaw1622/rui/FEUP-LBAW/proto/';
 
 function initserchname() {
 	var name = $('#searched-words').text();
@@ -13,6 +14,16 @@ function addeventChange() {
 		var name = $('#serch-input').val();
 		$('#searched-words').text(name);
 		$('.page-header h1').text("Search Results for \"" + name + "\"");
+		initUsersReloader();
 	});
 }
 
+function initTweetReloader() {
+	var name = $('#serch-input').val();
+    $.getJSON(BASE_URL + "actions/user/serchusers.php", {page: '0', serch: name, asc: 'ASC'}, function(data) {
+		console.log(data);
+      $.each(data, function(i, asc) {
+		  console.log(asc);
+      });
+    });
+}
