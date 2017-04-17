14efea9fca85b48065a5ca318309cd35d15bdf8f
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 23:43:17 +0100

    asd

--- proto/database/user.php
+++ proto/database/user.php
@@ -9,9 +9,9 @@
    function getSearchUsers($page, $name, $asc) {
     global $conn;
 	$param = "%$name%";
-    $stmt = $conn->prepare('SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url
+    $stmt = $conn->prepare('SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url, public.Authenticated_User.username
 							FROM public.Authenticated_User INNER JOIN public.Users ON (public.Authenticated_User.user_id = public.Users.user_id)
-							WHERE (upper(last_name) LIKE upper(?) OR upper(first_name) LIKE upper(?)) 
+							WHERE (upper(last_name) LIKE upper(?) OR upper(first_name) LIKE upper(?) OR upper(username) LIKE upper(?)) 
 							ORDER BY first_name ' . $asc . 
 							' LIMIT 10 OFFSET ? * 10;');
     $stmt->execute(array($param, $param, $page));
