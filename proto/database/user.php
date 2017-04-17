08719956e3081e35331701452d8fcdf03654ac2e
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 03:23:31 +0100

    test

--- proto/database/user.php
+++ proto/database/user.php
@@ -8,16 +8,12 @@
   
    function getSearchUsers($page, $name) {
     global $conn;
-	echo "SELECT  public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url
+
+    $stmt = $conn->prepare('SELECT  public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url
 							FROM public.Authenticated_User, public.Users
-							WHERE public.Authenticated_User.user_id = public.Users.user_id AND public.Users.first_name LIKE '%$name%'
-							LIMIT 10 OFFSET $page * 10;";
-    /*$stmt = $conn->prepare('SELECT  public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url
-							FROM public.Authenticated_User, public.Users
-							WHERE public.Authenticated_User.user_id = public.Users.user_id AND public.Users.first_name LIKE \'%?%\'
-							LIMIT 10 OFFSET ? * 10;');*/
-	$stmt = $conn->prepare("SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url FROM public.Authenticated_User, public.Users WHERE public.Users.first_name LIKE '%c%' LIMIT 10 OFFSET 1 * 10;");
-    $stmt->execute();
+							WHERE public.Users.first_name LIKE \'%?%\'
+							LIMIT 10 OFFSET ? * 10;');
+    $stmt->execute(array($name, $page));
     return $stmt->fetchAll();
   }
 ?>
