d8d50bb7d832876b1995ccc5f4a296bcbc117db5
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 05:24:11 +0100

    twest

--- proto/database/event.php
+++ proto/database/event.php
@@ -29,11 +29,6 @@
 		$stringnNOP = "name"; //"name, price" falta implementar o price
 	else
 		$stringnNOP = "name"; //"price, name" falta implementar o price
-	echo 'SELECT *
-							FROM public.Event
-							WHERE name LIKE ?' . $stringfreee . $stringpaid .
-							' ORDER BY ' . $stringnNOP . ' ' . $asc . 
-							' LIMIT 10 OFFSET ? * 10;';
     $stmt = $conn->prepare('SELECT *
 							FROM public.Event
 							WHERE name LIKE ?' . $stringfreee . $stringpaid .
