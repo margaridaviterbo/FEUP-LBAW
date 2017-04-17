833ba24a3d2f69063ca6654b09f5eef27cab7cbe
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 23:19:31 +0100

    rwa

--- proto/database/user.php
+++ proto/database/user.php
@@ -11,7 +11,7 @@
 	$param = "%$name%";
     $stmt = $conn->prepare('SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url
 							FROM public.Authenticated_User INNER JOIN public.Users ON (public.Authenticated_User.user_id = public.Users.user_id)
-							WHERE (last_name LIKE upper(?) OR first_name LIKE upper(?)) 
+							WHERE (upper(last_name) LIKE upper(?) OR upper(first_name) LIKE upper(?)) 
 							ORDER BY first_name ' . $asc . 
 							' LIMIT 10 OFFSET ? * 10;');
     $stmt->execute(array($param, $param, $page));
