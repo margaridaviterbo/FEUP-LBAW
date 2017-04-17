914a78b2acd4a7fcb38d954d34f3d5409081b100
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-17 04:01:55 +0100

    test

--- proto/pages/user/search.php
+++ proto/pages/user/search.php
@@ -208,7 +208,7 @@
             </div>
 			
 			<?php 
-			print_r(getSearchUsers(1, 'Bonnie'));
+			print_r(getSearchUsers(1, 'a'));
 			?>
             <div class="container-fluid user-card-medium">
               <p class="titulo-card">festa123</p>
