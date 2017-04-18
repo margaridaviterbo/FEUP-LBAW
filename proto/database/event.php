15ec713f922ff615362ae7cd785feb1bdd347456
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-18 22:25:42 +0100

    asd

--- proto/database/event.php
+++ proto/database/event.php
@@ -30,11 +30,6 @@
 	}else{
 		$stringnNOP = "name"; //"price, name" falta implementar o price
 	}
-	echo 'SELECT *
-							FROM public.Event  INNER JOIN public.Localization ON (public.Event.local_id = public.Localization.local_id)
-							WHERE upper(name) LIKE upper(?)' . $stringfreee . $stringpaid .
-							' ORDER BY  ? ' . $asc .
-							' LIMIT 10 OFFSET ? * 10;';
     $stmt = $conn->prepare('SELECT *
 							FROM public.Event  INNER JOIN public.Localization ON (public.Event.local_id = public.Localization.local_id)
 							WHERE upper(name) LIKE upper(?)' . $stringfreee . $stringpaid .
