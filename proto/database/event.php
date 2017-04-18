94e8b1ff666de64babca64e19c92cd6d41ef3efe
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-18 18:13:36 +0100

    asd

--- proto/database/event.php
+++ proto/database/event.php
@@ -29,17 +29,12 @@
 		$stringnNOP = "name"; //"name, price" falta implementar o price
 	else
 		$stringnNOP = "name"; //"price, name" falta implementar o price
-	echo 'SELECT *
-							FROM public.Event  INNER JOIN public.Localization ON (public.Event.local_id = public.Localization.local_id)
-							WHERE upper(name) LIKE upper(?)' . $stringfreee . $stringpaid .
-							' ORDER BY ' . $stringnNOP . ' ' . $asc . 
-							' LIMIT 10 OFFSET ? * 10;';
     $stmt = $conn->prepare('SELECT *
 							FROM public.Event  INNER JOIN public.Localization ON (public.Event.local_id = public.Localization.local_id)
 							WHERE upper(name) LIKE upper(?)' . $stringfreee . $stringpaid .
 							' ORDER BY ' . $stringnNOP . ' ' . $asc . 
 							' LIMIT 10 OFFSET ? * 10;');
     $stmt->execute(array($param, $page));
-    echo $stmt->fetchAll();
+    return $stmt->fetchAll();
   }
 ?>
