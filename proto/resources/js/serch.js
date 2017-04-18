a9d86405e69733bcdb0004e7203fb24f62888fa9
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-18 03:40:13 +0100

    asd

--- proto/resources/js/serch.js
+++ proto/resources/js/serch.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
 });
 BASE_URL = 'http://gnomo.fe.up.pt/~lbaw1622/rui/FEUP-LBAW/proto/';
 
-var canUpdate = true;
+var canUpdateuser = true;
+var canUpdateEvent = true;
 var askedToUpdate = false;
 
 function initserchname() {
@@ -25,7 +26,7 @@ function addeventChange() {
 }
 
 function addorderlisteners() {
-	$('.tabOptionsUsers input').on('change', function() {
+	$('.tabOptions input').on('change', function() {
 		askedToUpdate = true;
 		testCanUpdate();
 	});
@@ -33,13 +34,14 @@ function addorderlisteners() {
 
 function testCanUpdate() {
 	if(askedToUpdate){
-		if(canUpdate){
+		if(canUpdateuser && canUpdateEvent){
 			$('#usersPesq .usercadssech').html("");
 			var name = $('#serch-input').val();
 			var ascUser = $('input[name=alfa-order-users]:checked').val();
 			var ascEvent = $('input[name=alfa-order-event]:checked').val();
 			var byEvent = $('input[name=type-order-event]:checked').val();
 			doajaxusercall('0', name, ascUser);
+			doajaxeventcall('0', name, true, true, true, 'ASC')
 			askedToUpdate = false;
 		}
 	}
@@ -47,7 +49,7 @@ function testCanUpdate() {
 
 function doajaxusercall(page, name, asc) {
 	  var ind = 0;
-	  canUpdate = false;
+	  canUpdateuser = false;
 	  $.getJSON(BASE_URL + "actions/user/serchusers.php", {page: page, serch: name, asc: asc}, function(data) {
       $.each(data, function(i, asc) {
 		  ind += 1;
@@ -67,6 +69,44 @@ function doajaxusercall(page, name, asc) {
             '</div>');
       });
 		$('#tabs .button-users').html('Users (' + ind + ')');
-		canUpdate = true;
+		canUpdateuser = true;
+    });
+}
+
+function doajaxeventcall(page, name, free, paid, nameOrPrice, asc) {
+	  var ind = 0;
+	  canUpdateEvent = false;
+	  $.getJSON(BASE_URL + "actions/user/serchevents.php", {page: page, serch: name, free: free, paid: paid, nameOrPrice: nameOrPrice, asc: asc}, function(data) {
+      $.each(data, function(i, asc) {
+		  ind += 1;
+		 $('#eventosPesq .eventcadssech').append(
+		    '<div class="container-fluid event-card-medium">' +
+              '<p class="titulo-card">Sessão de Demonstração para o Desenvolvimento em Comunicação</p>' +
+              '<div class="row">' +
+                '<div class="col-sm-3">' +
+                  '<img src="../resources/images/1.jpg" />' +
+                '</div>' +
+                '<div class="col-sm-9">' +
+                  '<p class="text-card"> Quinta, 9 de Março às 19:45</p>' +
+                  '<p class="text-card">ISG</p>' +
+                  '<p class="text-card">Gratuito</p>' +
+                  '<div class="container-fluid">' + 
+                    '<div class="row">' +
+                      '<button type="button" class="btn btn-default col-sm-5">See More...</button>' +
+                      '<div class="classifica-card col-sm-7">' +
+                        '<i class="fa fa-star fa-2x" aria-hidden="true"></i>' +
+                        '<i class="fa fa-star fa-2x" aria-hidden="true"></i>' +
+                        '<i class="fa fa-star fa-2x" aria-hidden="true"></i>' +
+                        '<i class="fa fa-star-o fa-2x" aria-hidden="true"></i>' +
+                        '<i class="fa fa-star-o fa-2x" aria-hidden="true"></i>' +
+                      '</div>' +
+                    '</div>' +
+                  '</div>' +
+                '</div>' +
+              '</div>' +
+            '</div>');
+      });
+		$('#tabs .button-events').html('Events (' + ind + ')');
+		canUpdateEvent = true;
     });
 }
