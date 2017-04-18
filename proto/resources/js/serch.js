e3611ca68cd937fb2349cdd8cf2a773a50b98f5b
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-18 17:27:57 +0100

    awd

--- proto/resources/js/serch.js
+++ proto/resources/js/serch.js
@@ -79,6 +79,7 @@ function doajaxusercall(page, name, asc) {
 function doajaxeventcall(page, name, free, paid, nameOrPrice, asc) {
 	  var ind = 0;
 	  canUpdateEvent = false;
+	  console.log(free + " e " + paid);
 	  $.getJSON(BASE_URL + "actions/user/serchevents.php", {page: page, serch: name, free: free, paid: paid, nameOrPrice: nameOrPrice, asc: asc}, function(data) {
       $.each(data, function(i, asc) {
 		  ind += 1;
