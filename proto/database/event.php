7695dea15cdd9c3f31def29cb91eea85cfdc118e
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-20 00:21:01 +0100

    asd

--- proto/database/event.php
+++ proto/database/event.php
@@ -29,12 +29,12 @@
 		$stringnNOP = "name"; //"name, price" falta implementar o price
 	}else{
 		$stringnNOP = "name"; //"price, name" falta implementar o price
-	} //public.City.name AS cityName, public.Localization.street, public.Meta_Event.name AS name, public.Meta_Event.photo_url, public.Meta_Event.beginning_date, public.Meta_Event.ending_date, public.Meta_Event.free
+	} //public.City.name AS cityName, public.Localization.street, public.Event.name AS name, public.Event.photo_url, public.Event.beginning_date, public.Event.ending_date, public.Event.free
     $stmt = $conn->prepare('SELECT *
 							FROM ((public.Event 
 								 INNER JOIN public.Localization ON (public.Event.local_id = public.Localization.local_id))
 								 INNER JOIN public.City ON (public.City.city_id = public.Localization.city_id))
-							WHERE upper(public.Meta_Event.name) LIKE upper(?)' . $stringfreee . $stringpaid .
+							WHERE upper(public.Event.name) LIKE upper(?)' . $stringfreee . $stringpaid .
 							' ORDER BY ' . $stringnNOP . ' ' . $asc .
 							' LIMIT 10 OFFSET ? * 10;');
     $stmt->execute(array($param, $page));
