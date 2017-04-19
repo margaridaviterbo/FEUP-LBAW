0335fdc54fb0dd6695ca8cebc73af65743a73b51
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-20 00:48:26 +0100

    asd

--- proto/database/event.php
+++ proto/database/event.php
@@ -29,15 +29,20 @@
 		$stringnNOP = "public.Event.name"; //"name, price" falta implementar o price
 	}else{
 		$stringnNOP = "public.Event.name"; //"price, name" falta implementar o price
-	} //public.City.name AS cityName, public.Localization.street, public.Event.name AS name, public.Event.photo_url, public.Event.beginning_date, public.Event.ending_date, public.Event.free
-    $stmt = $conn->prepare('SELECT public.City.name AS cityName, public.Localization.street, public.Event.name AS name, public.Event.photo_url, public.Event.beginning_date, public.Event.ending_date, public.Event.free
+	}
+    /*$stmt = $conn->prepare('SELECT public.City.name AS cityName, public.Localization.street, public.Event.name AS name, public.Event.photo_url, public.Event.beginning_date, public.Event.ending_date, public.Event.free, public.Event.event_id
 							FROM ((public.Event 
 								 INNER JOIN public.Localization ON (public.Event.local_id = public.Localization.local_id))
 								 INNER JOIN public.City ON (public.City.city_id = public.Localization.city_id))
 							WHERE upper(public.Event.name) LIKE upper(?)' . $stringfreee . $stringpaid .
 							' ORDER BY ' . $stringnNOP . ' ' . $asc .
 							' LIMIT 10 OFFSET ? * 10;');
-    $stmt->execute(array($param, $page));
+    $stmt->execute(array($param, $page));*/
+	$stmt = $conn->prepare('SELECT name, public.Event.event_id, AVG(evaluation)
+							FROM ((public.Rate 
+								 INNER JOIN public.Event_Content ON (public.Rate.event_content_id = public.Event_Content.event_content_id))
+								 INNER JOIN public.Event ON (public.Event.event_id = public.Event_Content.event_id))');
+    $stmt->execute();
     return $stmt->fetchAll();
   }
 ?>
