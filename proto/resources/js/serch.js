e7bf20af91f5185c9cd7ea016dc1c9f5efaebd15
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-20 01:41:26 +0100

    asd

--- proto/resources/js/serch.js
+++ proto/resources/js/serch.js
@@ -122,11 +122,11 @@ function doajaxeventcall(page, name, free, paid, nameOrPrice, asci) {
 		  }
 		  rateEvent = Math.floor(rateEvent);
 		  console.log(rateEvent);
-		  rateString.concat('<i class="fa fa-star fa-2x" aria-hidden="true"></i>'.repeat(rateEvent));
+		  rateString.concat("<i class=\"fa fa-star fa-2x\" aria-hidden=\"true\"></i>".repeat(rateEvent));
 		  console.log(rateString);
 		  rateString.concat('<i class="fa fa-star-o fa-2x" aria-hidden="true"></i>'.repeat(5-rateEvent));
 		  console.log(rateString);
-		  console.log('aeiou'.repeat(5));
+		  console.log('aei"ou'.repeat(5));
 		 $('#eventosPesq .eventcadssech').append(
 		    '<div class="container-fluid event-card-medium">' +
               '<p class="titulo-card">' + asc.name + '</p>' +
