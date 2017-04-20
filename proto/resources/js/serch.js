998273270ff1825afc34572e49f5488fd3d06f34
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-20 02:43:25 +0100

    asd

--- proto/resources/js/serch.js
+++ proto/resources/js/serch.js
@@ -166,7 +166,7 @@ function doajaxeventcall(page, name, free, paid, nameOrPrice, asci) {
 
 function finMoreUsers(page) {
 		if(canUpdateuser && canUpdateEvent){
-			console.log($('.find-more-users').html());
+			console.log($('.find-more-users').parent().html());
 			var name = $('#serch-input').val();
 			if(name == ''){
 				$('#tabs .button-users').html('Users (0)');
