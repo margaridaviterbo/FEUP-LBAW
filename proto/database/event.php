b96b2d2f21bf1ce104511e0920149498225f26bb
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-22 16:54:33 +0100

    sdf

--- proto/database/event.php
+++ proto/database/event.php
@@ -28,4 +28,48 @@ function getEventsCreatedByUser($username){
     $stmt->execute(array($username));
     return $stmt->fetchAll();
 }
+
+  /**
+  $page, numero da pagina
+  $name, nome do evento a procurar
+  $free, true se procurar em free
+  $paid, true se procurar em paid
+  $nameOrPrice, true se nome false se price
+  $asc, ASC ou DESC
+  */
+   function getSearchEvents($page, $name, $free, $paid, $nameOrPrice, $asc) {
+    global $conn;
+	$param = "%$name%";
+	$stringfreee = "";
+	$stringpaid = "";
+	if(!($free))
+		$stringfreee = " AND free = false";
+	
+	if(!($paid))
+		$stringpaid = " AND free = true";
+	
+	if($nameOrPrice){ //name
+		$stringnNOP = "name $asc"; //"name, price" falta implementar o price
+	}else{
+		$stringnNOP = "name $asc"; //"price, name" falta implementar o price
+	}
+    $stmt = $conn->prepare('SELECT cityName, street, name, photo_url, beginning_date, ending_date, free, eventInfo.eveId, rate
+							FROM
+								(SELECT public.City.name AS cityName, public.Localization.street, public.Event.name AS name, public.Event.photo_url, public.Event.beginning_date, public.Event.ending_date, public.Event.free, public.Event.event_id AS eveId
+								FROM ((public.Event 
+									 INNER JOIN public.Localization ON (public.Event.local_id = public.Localization.local_id))
+									 INNER JOIN public.City ON (public.City.city_id = public.Localization.city_id))
+								WHERE upper(public.Event.name) LIKE upper(?)' . $stringfreee . $stringpaid .
+								') AS eventInfo,
+								(SELECT public.Event.event_id AS avgEvId, AVG(evaluation) as rate
+								FROM ((public.Rate 
+									 INNER JOIN public.Event_Content ON (public.Rate.event_content_id = public.Event_Content.event_content_id))
+									 RIGHT JOIN public.Event ON (public.Event.event_id = public.Event_Content.event_id))
+								GROUP BY public.Event.event_id) AS aveInfo
+							WHERE (eventInfo.eveId = aveInfo.avgEvId)
+							ORDER BY ' . $stringnNOP .
+							' LIMIT 10 OFFSET ? * 10;');
+    $stmt->execute(array($param, $page));
+    return $stmt->fetchAll();
+  }
 ?>
--- proto/database/user.php
+++ proto/database/user.php
@@ -113,4 +113,22 @@
         $stmt->execute(array($username, sha1($password)));
         return $stmt->fetch() == true;
     }
+	  function getAllUsers($page) {
+    global $conn;
+    $stmt = $conn->prepare('SELECT * FROM public.Users LIMIT 10 OFFSET ? * 10;');
+    $stmt->execute(array($page));
+    return $stmt->fetchAll();
+  }
+  
+   function getSearchUsers($page, $name, $asc) {
+    global $conn;
+	$param = "%$name%";
+    $stmt = $conn->prepare('SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url, public.Authenticated_User.username
+							FROM public.Authenticated_User INNER JOIN public.Users ON (public.Authenticated_User.user_id = public.Users.user_id)
+							WHERE (upper(last_name) LIKE upper(?) OR upper(first_name) LIKE upper(?) OR upper(username) LIKE upper(?)) 
+							ORDER BY first_name ' . $asc . 
+							' LIMIT 10 OFFSET ? * 10;');
+    $stmt->execute(array($param, $param, $param, $page));
+    return $stmt->fetchAll();
+  }
 ?>
