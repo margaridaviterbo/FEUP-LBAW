d48df64c137d4097ed71e10079496d5cc330ce78
Catarina24 <aniratac.correia@gmail.com>
2017-05-29 00:26:35 +0100

    hosts added

--- proto/database/host.php
+++ proto/database/host.php
@@ -6,4 +6,12 @@ function addHost($userId, $eventId){
     $stmt->execute(array($userId, $eventId));
 }
 
+function getHosts($eventId){
+    global $conn;
+    $stmt = $conn->prepare('SELECT authenticated_user.username, authenticated_user.photo_url FROM public.authenticated_user
+                            INNER JOIN public.host ON public.host.user_id = public.authenticated_user.user_id
+                            WHERE public.host.meta_event_id = ?');
+    $stmt->execute(array($eventId));
+    return $stmt->fetchAll();
+}
 ?>
