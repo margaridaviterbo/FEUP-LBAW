cd74dfb7e61a54be31cb109287cff3b89a8ec34b
Catarina24 <aniratac.correia@gmail.com>
2017-05-28 01:44:53 +0100

    changes to database

--- proto/database/sql/inserts-test.sql
+++ proto/database/sql/inserts-test.sql
@@ -7,5 +7,7 @@ INSERT INTO public.category(name) VALUES ('food&drinks');
 INSERT INTO public.category(name) VALUES ('music');
 
 insert into public.users (first_name, last_name, email, nif) values ('Catarina', 'Correia', 'cat@gmail.com',    123456789);
+insert into public.users (first_name, last_name, email, nif) values ('Maria', 'Joana', 'joana@gmail.com',    123456788);
 
 insert into public.authenticated_user (user_id, username, password, user_state) values (1, 'catarina24', '8bc5de83cf1daf79ed5b2f13f93d7c05d01d0388', 'active');
+insert into public.authenticated_user (user_id, username, password, user_state) values (2, 'joana123', '8bc5de83cf1daf79ed5b2f13f93d7c05d01d0388', 'active');
